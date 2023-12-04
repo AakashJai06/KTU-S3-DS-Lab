84a023b274042dd4a9608ae1720931ffd5bce3c6
AakashJai06 <127650255+AakashJai06@users.noreply.github.com>
2023-12-04 21:27:26 +0530

    Update sparseSum.c

--- Cycle1/sparseSum.c
+++ Cycle1/sparseSum.c
@@ -1,84 +1,65 @@
 #include <stdio.h>
-void main() {
-    int a[100][3], b[100][3], c[100][3], n0, n1;
-
-    printf("Enter the number of non-zero elements in the matrices: ");
-    scanf("%d %d", &n0, &n1);
-
-    printf("Enter Sparse Representation of the 1st matrix:\n");
-    for (int i = 0; i < n0; i++) {
-        for (int j = 0; j < 3; j++) {
-            scanf("%d", &a[i][j]);
+struct tuple{
+    int row;
+    int col;
+    int val;
+}sparse1[50],sparse2[50],sparse3[100];
+int k=0;
+void insert(struct tuple sparse[50],int n){
+    printf("<--Elements in tuple form-->\n");
+    for(int i=0;i<n;i++){
+        scanf("%d %d %d",&sparse[i].row,&sparse[i].col,&sparse[i].val);
+    }
+}
+void display(struct tuple sparse[50],int n){
+    for(int i=0;i<n;i++)
+    {
+        printf("%5d%5d%5d\n",sparse[i].row,sparse[i].col,sparse[i].val);
+    }
+}
+void add(struct tuple sparse1[50],struct tuple sparse2[50],struct tuple sparse3[100],int n1,int n2){
+    int i=0,j=0;
+    while(i<n1 && j<n2){
+        if(sparse1[i].row == sparse2[j].row && sparse1[i].col == sparse2[i].col){
+            sparse3[k].row = sparse1[i].row;
+            sparse3[k].col = sparse1[i].col;
+            sparse3[k].val = sparse1[i].val+sparse2[j].val;
+            i++;j++;k++;
+        }
+        else if(sparse1[i].row <sparse2[j].row || sparse1[i].row == sparse2[j].row && sparse1[i].col<sparse2[j].col ){
+            sparse3[k].row = sparse1[i].row;
+            sparse3[k].col = sparse1[i].col;
+            sparse3[k].val = sparse1[i].val;
+            i++;k++;
+        }
+        else{
+            sparse3[k].row = sparse2[j].row;
+            sparse3[k].col = sparse2[j].col;
+            sparse3[k].val = sparse2[j].val;
+            j++;k++;
         }
     }
-
-    printf("Enter Sparse Representation of the 2nd matrix:\n");
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < 3; j++) {
-            scanf("%d", &b[i][j]);
-        }
+    while(i<n1){
+            sparse3[k].row = sparse1[i].row;
+            sparse3[k].col = sparse1[i].col;
+            sparse3[k].val = sparse1[i].val;
+            i++;k++;
     }
-
-    printf("\nRow\tColumn\tValue\n");
-    for (int i = 0; i < n0; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d\t", a[i][j]);
-        }
-        printf("\n");
+    while(j<n2){
+            sparse3[k].row = sparse2[j].row;
+            sparse3[k].col = sparse2[j].col;
+            sparse3[k].val = sparse2[j].val;
+            j++;k++;
     }
-
-    printf("\nRow\tColumn\tValue\n");
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d\t", b[i][j]);
-        }
-        printf("\n");
-    }
-
-    int i = 0, j = 0, k = 0;
-
-    while (i < n0 && j < n1) {
-        if (a[i][0] < b[j][0] || (a[i][0] == b[j][0] && a[i][1] < b[j][1])) {
-            c[k][0] = a[i][0];
-            c[k][1] = a[i][1];
-            c[k][2] = a[i][2];
-            i++;
-        } else if (a[i][0] > b[j][0] || (a[i][0] == b[j][0] && a[i][1] > b[j][1])) {
-            c[k][0] = b[j][0];
-            c[k][1] = b[j][1];
-            c[k][2] = b[j][2];
-            j++;
-        } else {
-            c[k][0] = a[i][0];
-            c[k][1] = a[i][1];
-            c[k][2] = a[i][2] + b[j][2];
-            i++;
-            j++;
-        }
-
-        k++;
-    }
-    while (i < n0) {
-        c[k][0] = a[i][0];
-        c[k][1] = a[i][1];
-        c[k][2] = a[i][2];
-        i++;
-        k++;
-    }
-
-    while (j < n1) {
-        c[k][0] = b[j][0];
-        c[k][1] = b[j][1];
-        c[k][2] = b[j][2];
-        j++;
-        k++;
-    }
-
-    printf("\nRow\tColumn\tValue\n");
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d\t", c[i][j]);
-        }
-        printf("\n");
-    }
+}
+void main(){
+    int n1,n2;
+    printf("<-Enter Number of non zero elements->");
+    scanf("%d",&n1);
+    insert(sparse1,n1);
+    printf("<-Enter Number of non zero elements->");
+    scanf("%d",&n2);
+    insert(sparse2,n2);
+    add(sparse1,sparse2,sparse3,n1,n2);
+    display(sparse3,k);
 }
